053c246ff17c2be1ed4f8aff80c15df6f00a82c9
Andres Reyes <areyesdev@gmail.com>
2024-09-19 20:06:22 -0500

    Add: component phon, update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
-import MaxWidthWrapper from '../components/MaxWidthWrapper';
+import { Check, Star } from "lucide-react";
+import MaxWidthWrapper from "../components/MaxWidthWrapper";
+import Phone from "../components/Phone";
 
 export default function Home() {
   return (
@@ -9,9 +10,95 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/snake-q.png" alt="snake" className="w-full" />
+                <img src="/snake-2.png" alt="snake" className="w-full" />
               </div>
-              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">Your Image on a <span className="bg-green-600 px-2 text-white">Custom</span> Phone case</h1>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Tu imagen{" "}
+                <span className="bg-green-600 px-2 text-white">
+                  Personalizada
+                </span>{" "}
+                En una funda de Telefono.
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Captura tus recuerdos favoritos con los tuyos.{" "}
+                <span className="font-semibold">uno a uno</span> en tu funda de
+                telefono
+              </p>
+              <ul className="mt-8 space-y2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrik-0 text-green-600" />
+                    Alta-calidad, materiales duraderos
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrik-0 text-green-600" />5 años
+                    de garantia
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrik-0 text-green-600" />
+                    Soporte para modelos nuevos de IPhone
+                  </li>
+                </div>
+              </ul>
+
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  <img
+                    src="/users/user-1.png"
+                    alt="Imagen de usuario"
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <img
+                    src="/users/user-2.png"
+                    alt="Imagen de usuario"
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <img
+                    src="/users/user-3.png"
+                    alt="Imagen de usuario"
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <img
+                    src="/users/user-4.jpg"
+                    alt="Imagen de usuario"
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <img
+                    src="/users/user-5.jpg"
+                    alt="Imagen de usuario"
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100 object-cover"
+                  />
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1.250</span> Clientes
+                    Felices.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img
+                src="/your-image.png"
+                alt="imagen"
+                className="absolute w-40 lg:52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+              />
+              <img
+                src="/line.png"
+                alt="imagen"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+              />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
